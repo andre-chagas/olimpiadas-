9681b24ae41d1e4126243544ed656fbfb97288e3
André Chagas <84164855+andre-chagas@users.noreply.github.com>
2024-09-10 21:15:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados =  [
+    {
+        titulo: "Raissa Leal",
+        descricao: "Rayssa Leal, é uma skatista brasileira, conhecida como Fadinha, que se tornou a mais jovem medalhista olímpica da história do Brasil ao conquistar a prata nos Jogos Olímpicos de Tóquio 2020.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua força, determinação e talento. Bicampeã olímpica, ela é considerada a maior ginasta da história do Brasil",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca na categoria pesado. Ela conquistou diversas medalhas em competições nacionais e internacionais, representando o Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginástica ginastica ouro"
+    }
+
+];
+
